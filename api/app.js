f6df8c55db65309c0c30acd69a522780d6265289
Mikhail Sokolov <pilligrim17@yandex.ru>
2025-07-06 23:41:34 +0300

    done!

--- api/app.js
+++ api/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const session = require("express-session");
 const Keycloak = require("keycloak-connect");
 const cors = require("cors");
 
@@ -8,13 +7,11 @@ const port = 8000;
 
 app.use(cors());
 
-const memoryStore = new session.MemoryStore();
-
 const keycloakConfig = {
   realm: "reports-realm",
   clientId: "reports-api",
   "bearer-only": true,
-  "auth-server-url": process.env.KEYCLOAK_URL || "http://localhost:8080/",
+  "auth-server-url": process.env.KEYCLOAK_URL || "http://keycloak:8080/",
   "ssl-required": "external",
   resource: "reports-api",
   credentials: {
@@ -22,21 +19,36 @@ const keycloakConfig = {
   },
 };
 
-const keycloak = new Keycloak({ store: memoryStore }, keycloakConfig);
+const keycloak = new Keycloak({}, keycloakConfig);
+
+app.use((req, res, next) => {
+  console.log(`[REQUEST] ${req.method} ${req.url}`);
+  console.log(`Headers:`, req.headers);
+  next();
+});
 
 app.use(keycloak.middleware());
 
-app.get("/public", (req, res) => {
-  res.send("Доступно без авторизации");
+app.use((req, res, next) => {
+  console.log(`[BEFORE protect()] ${req.method} ${req.url}`);
+  next();
 });
 
-app.use(keycloak.protect(), (req, res, next) => {
-  console.log("kauth: ", req.kauth);
+app.use(keycloak.protect());
+
+app.use((req, res, next) => {
+  console.log(`[AFTER protect()] ${req.method} ${req.url}`);
+  console.log("Request headers:", req.headers);
+  console.log("Keycloak config:", keycloakConfig);
+  console.log("kauth:", req.kauth);
+  if (req.kauth && req.kauth.grant) {
+    console.log("Token content:", req.kauth.grant.access_token.content);
+  }
   next();
 });
 
 app.get("/reports", (req, res) => {
-  // console.log("User:", req.kauth.grant.access_token.content);
+  console.log(`Serving /reports`);
   res.json({ message: "reports" });
 });
 
@@ -44,7 +56,12 @@ app.use("*", (req, res) => {
   res.send("Not found!");
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
+  // const response = await fetch(keycloakConfig["auth-server-url"]);
+  // console.log(response);
+
+  console.log(process.env);
+
   console.log(JSON.stringify(keycloakConfig, null, 2));
   console.log(keycloak);
   console.log(`Listening on port ${port}.`);
